d37d8d6a45f3563a3cd82b6586999f2f6cdd64d9
Hannah Abraham <hsabraham28@berkeley.edu>
2020-07-18 05:04:28 -0500

    Updates

---
+++ precache-manifest.2c3faaf8f96cf8a54b081f6a7b7f465a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b9573c0d7286788e6e9ab41f05f406",
+    "url": "/bear-virtual-sproul/index.html"
+  },
+  {
+    "revision": "247b52053f4bd5b3308c",
+    "url": "/bear-virtual-sproul/static/css/2.dad1575f.chunk.css"
+  },
+  {
+    "revision": "2c92087118dcb2998641",
+    "url": "/bear-virtual-sproul/static/css/main.19713a12.chunk.css"
+  },
+  {
+    "revision": "247b52053f4bd5b3308c",
+    "url": "/bear-virtual-sproul/static/js/2.052909c6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/bear-virtual-sproul/static/js/2.052909c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c92087118dcb2998641",
+    "url": "/bear-virtual-sproul/static/js/main.8066b0fb.chunk.js"
+  },
+  {
+    "revision": "2d2300f5b496ea3909e3",
+    "url": "/bear-virtual-sproul/static/js/runtime-main.12c3f87d.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/bear-virtual-sproul/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/bear-virtual-sproul/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/bear-virtual-sproul/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/bear-virtual-sproul/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/bear-virtual-sproul/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/bear-virtual-sproul/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/bear-virtual-sproul/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/bear-virtual-sproul/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/bear-virtual-sproul/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/bear-virtual-sproul/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/bear-virtual-sproul/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/bear-virtual-sproul/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/bear-virtual-sproul/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/bear-virtual-sproul/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/bear-virtual-sproul/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/bear-virtual-sproul/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/bear-virtual-sproul/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "7232e319bda279f1cd594394cde3a4e4",
+    "url": "/bear-virtual-sproul/static/media/oski-big.7232e319.png"
+  }
+]);
