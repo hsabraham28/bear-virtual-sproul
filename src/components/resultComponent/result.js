e727ac7f9072738a4c0d4fc4da4118049335433e
Hannah Abraham <hsabraham28@berkeley.edu>
2020-07-25 18:20:34 -0500

    cardComponent added

--- src/components/resultComponent/result.js
+++ src/components/resultComponent/result.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBView, MDBIcon } from 'mdbreact';
 import oski from './oski-big.png';
+import ClubCard from './clubCardComponent/clubCard';
 
 console.log(oski);
 
@@ -16,183 +17,37 @@ class Result extends Component {
       <div className="result-section">
   <div className="rowOne">      
  <MDBRow>
+
       <MDBCol md='2'>
-        <MDBCard wide cascade>
-          <MDBView cascade>
-            <MDBCardImage
-              hover
-              overlay='white-slight'
-              className='card-img-top'
-              src={oski}
-              alt='Card cap'
-            />
-          </MDBView>
-
-          <MDBCardBody cascade className='text-center'>
-            <MDBCardTitle className='card-title'>
-              <strong>Club Name</strong>
-            </MDBCardTitle>
-
-
-            <MDBCardText>
-              Brief Introduction
-            </MDBCardText>
-
-            <MDBCol md='12' className='d-flex justify-content-center'>
-              
-            </MDBCol>
-          </MDBCardBody>
-        </MDBCard>
+      <ClubCard />
       </MDBCol>
 
       <MDBCol md='2'>
-        <MDBCard wide cascade>
-          <MDBView cascade>
-            <MDBCardImage
-              hover
-              overlay='white-slight'
-              className='card-img-top'
-              src={oski}
-              alt='Card cap'
-            />
-          </MDBView>
-
-          <MDBCardBody cascade className='text-center'>
-            <MDBCardTitle className='card-title'>
-              <strong>Club Name</strong>
-            </MDBCardTitle>
-
-
-            <MDBCardText>
-              Brief Introduction
-            </MDBCardText>
-
-            <MDBCol md='12' className='d-flex justify-content-center'>
-              
-            </MDBCol>
-          </MDBCardBody>
-        </MDBCard>
+      <ClubCard />
       </MDBCol>
 
       <MDBCol md='2'>
-        <MDBCard wide cascade>
-          <MDBView cascade>
-            <MDBCardImage
-              hover
-              overlay='white-slight'
-              className='card-img-top'
-              src={oski}
-              alt='Card cap'
-            />
-          </MDBView>
-
-          <MDBCardBody cascade className='text-center'>
-            <MDBCardTitle className='card-title'>
-              <strong>Club Name</strong>
-            </MDBCardTitle>
-
-
-            <MDBCardText>
-              Brief Introduction
-            </MDBCardText>
-
-            <MDBCol md='12' className='d-flex justify-content-center'>
-              
-            </MDBCol>
-          </MDBCardBody>
-        </MDBCard>
+      <ClubCard />
       </MDBCol>
+
     </MDBRow>
     </div>
 
 
     <MDBRow>
+
       <MDBCol md='2'>
-        <MDBCard wide cascade>
-          <MDBView cascade>
-            <MDBCardImage
-              hover
-              overlay='white-slight'
-              className='card-img-top'
-              src={oski}
-              alt='Card cap'
-            />
-          </MDBView>
-
-          <MDBCardBody cascade className='text-center'>
-            <MDBCardTitle className='card-title'>
-              <strong>Club Name</strong>
-            </MDBCardTitle>
-
-
-            <MDBCardText>
-              Brief Introduction
-            </MDBCardText>
-
-            <MDBCol md='12' className='d-flex justify-content-center'>
-              
-            </MDBCol>
-          </MDBCardBody>
-        </MDBCard>
+      <ClubCard />
       </MDBCol>
 
       <MDBCol md='2'>
-        <MDBCard wide cascade>
-          <MDBView cascade>
-            <MDBCardImage
-              hover
-              overlay='white-slight'
-              className='card-img-top'
-              src={oski}
-              alt='Card cap'
-            />
-          </MDBView>
-
-          <MDBCardBody cascade className='text-center'>
-            <MDBCardTitle className='card-title'>
-              <strong>Club Name</strong>
-            </MDBCardTitle>
-
-
-            <MDBCardText>
-              Brief Introduction
-            </MDBCardText>
-
-            <MDBCol md='12' className='d-flex justify-content-center'>
-              
-            </MDBCol>
-          </MDBCardBody>
-        </MDBCard>
+      <ClubCard />
       </MDBCol>
 
       <MDBCol md='2'>
-        <MDBCard wide cascade>
-          <MDBView cascade>
-            <MDBCardImage
-              hover
-              overlay='white-slight'
-              className='card-img-top'
-              src={oski}
-              alt='Card cap'
-            />
-          </MDBView>
-
-          <MDBCardBody cascade className='text-center'>
-            <MDBCardTitle className='card-title'>
-              <strong>Club Name</strong>
-            </MDBCardTitle>
-
-
-            <MDBCardText>
-              Brief Introduction
-            </MDBCardText>
-
-            <MDBCol md='12' className='d-flex justify-content-center'>
-              
-            </MDBCol>
-          </MDBCardBody>
-        </MDBCard>
+      <ClubCard />
       </MDBCol>
+      
     </MDBRow>
       
 
